c7c34b0815dd04fae3787147f395524772608bf3
Егор Гординский <yegorgordinskiy2002@gmail.com>
2023-02-07 03:41:07 +0800

    add FormAnswerToQuestion

---
+++ src/components/formAnswerToQuestion/FormAnswerToQuestion.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+import { Word } from "../../types/types";
+import SendButton from "../UI/buttonSend/SendButton";
+import Input from "../UI/input/Input";
+import styles from "./FormAnswerToQuestion.module.scss";
+
+interface FormAnswerToQuestionProps {
+  word: Word;
+  answer: string;
+  handlerChangeAnswer: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handlerClickSendAnswer: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  isShowAnswer: boolean;
+}
+
+const FormAnswerToQuestion: FC<FormAnswerToQuestionProps> = ({
+  word,
+  answer,
+  handlerChangeAnswer,
+  handlerClickSendAnswer,
+  isShowAnswer,
+}) => {
+  return (
+    <form className={styles.form}>
+      {isShowAnswer && (
+        <>
+          <h2>Упс... Неверно!</h2>
+          <p>
+            Не расстрайвайся! Прочти несколько раз термин и запиши его еще раз в
+            ответ
+          </p>
+          <p>Термин: {word.term}</p>
+          <p>Транскрипция: {word.transcription}</p>
+        </>
+      )}
+      <p>{word.definition}</p>
+      <div className={styles.send}>
+        <Input value={answer} onChange={handlerChangeAnswer} type="text" />
+        <SendButton onClick={handlerClickSendAnswer} />
+      </div>
+    </form>
+  );
+};
+
+export default FormAnswerToQuestion;
